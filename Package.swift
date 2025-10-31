736ffe7d2cd2c5d6d26c79dee770908795755102
movaman <asaf.m@gmail.com>
2025-10-31 17:11:52 +0200

    add package file

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ImageDownloaderFramework",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "ImageDownloaderFramework",
+            targets: ["ImageDownloaderFramework"]),
+    ],
+    dependencies: [
+        // Add dependencies if needed
+    ],
+    targets: [
+        .target(
+            name: "ImageDownloaderFramework",
+            dependencies: [],
+            path: "Sources/ImageDownloaderFramework"
+        ),
+        .testTarget(
+            name: "ImageDownloaderFrameworkTests",
+            dependencies: ["ImageDownloaderFramework"],
+            path: "Tests/ImageDownloaderFrameworkTests"
+        ),
+    ]
+)
